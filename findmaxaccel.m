f5a42213c4df4c4e8388d7472fc5b3ef0d73f81c
Leo Berman <105498120+Leo-Berman@users.noreply.github.com>
2023-10-17 15:16:44 -0400

    asd

--- findmaxaccel.m
+++ findmaxaccel.m
@@ -2,11 +2,11 @@
 % 10/10/2023
 % usage: max variable = findmaxaccel(input your transfer function, input your t
 % domain input,input your title as 'title')
-function[maxim]=findmaxaccel(transfunc,input,string)
+function[maxim]=findmaxaccel(transfunc,in,string)
     syms t
     % 10 second linspace
     
-
+    input=diff(in);
     % hold on
     % Convolves the input with the output
     out = ilaplace(transfunc*laplace(input))*heaviside(t);
